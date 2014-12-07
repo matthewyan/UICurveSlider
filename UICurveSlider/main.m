14110b983a817862a57d60de94c4d24dbcb10247
matthewyan(闫猛) <matthewyan@tencent.com>
2014-12-07 22:19:00 +0800

    Initial Commit

---
+++ UICurveSlider/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UICurveSlider
+//
+//  Created by matthew on 14-12-7.
+//  Copyright (c) 2014年 Matthew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
